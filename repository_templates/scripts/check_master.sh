99fd6f641429f9543199b7e14bf9064059f31b90
Martin Smith <martin@mbs3.org>
2018-05-18 14:16:10 -0400

    Clean up quoting and bash-isms

--- repository_templates/scripts/check_master.sh
+++ repository_templates/scripts/check_master.sh
@@ -2,29 +2,28 @@
 
 # standard paths
 WORKING_DIR=$(pwd)
-WORKSPACE_DIR=$WORKING_DIR/workspace
-LAYERS_DIR=$WORKING_DIR/layers
-LAYERS=$(find $LAYERS_DIR/* -type d -maxdepth 0 -exec basename '{}' \; | sort -n)
+WORKSPACE_DIR="$WORKING_DIR/workspace"
+LAYERS_DIR="$WORKING_DIR/layers"
+LAYERS=$(find "$LAYERS_DIR"/* -type d -maxdepth 0 -exec basename '{}' \; | sort -n)
 
 # be sure we know about the latest remote refs
 git fetch origin
 MASTER_REF=$(git rev-parse remotes/origin/master)
 
 # in the last hundred commits, is one of the parents in the current master?
-git log --pretty=format:'%H' -n 100 | grep -q $MASTER_REF
+git log --pretty=format:'%H' -n 100 | grep -q "$MASTER_REF"
 UPTODATE=$?
 
-if [ ${UPTODATE} -ne 0 ]
+if [ $UPTODATE -ne 0 ]
 then
   echo "Your branch is not up to date. Exiting."
-  exit ${UPTODATE}
 fi
 
-if [[ -f "$WORKSPACE_DIR/changed_layers" ]]; then
-  CHANGED_LAYERS=$(cat $WORKING_DIR/changed_layers)
+if [ -f "$WORKSPACE_DIR/changed_layers" ]; then
+  CHANGED_LAYERS=$(cat "$WORKING_DIR/changed_layers")
 else
-  CHANGED_LAYERS=$(git diff --name-only $MASTER_REF -- $LAYERS_DIR | awk -F "/" '{print $2}' | uniq)
-  echo $CHANGED_LAYERS > $WORKING_DIR/changed_layers
+  CHANGED_LAYERS=$(git diff --name-only "$MASTER_REF" -- "$LAYERS_DIR" | awk -F "/" '{print $2}' | sort -n | uniq)
+  echo $CHANGED_LAYERS > "$WORKING_DIR/changed_layers"
 fi
 
 echo "Changed layers: "
