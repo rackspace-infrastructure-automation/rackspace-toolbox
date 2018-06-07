1bc8a4099fd37448a8ab34051aca08a02aba0e59
Nate Bohn <nate.bohn@rackspace.com>
2018-06-07 15:54:55 -0500

    linting

--- aviator/route53/main.tf
+++ aviator/route53/main.tf
@@ -1,9 +1,10 @@
 # Internal Hosted Zone
 
 resource "aws_route53_zone" "internal_zone" {
-  name = "${var.zone_name}"
+  name    = "${var.zone_name}"
   comment = "Hosted zone for ${contains(var.env_list, var.zone_environment) ? var.zone_environment:"Development"}"
-  vpc_id = "${replace(var.target_vpc_id, "/^vpc-/", "") != var.target_vpc_id ? var.target_vpc_id:""}"
+  vpc_id  = "${replace(var.target_vpc_id, "/^vpc-/", "") != var.target_vpc_id ? var.target_vpc_id:""}"
+
   tags {
     ServiceProvider = "Rackspace"
   }
--- aviator/route53/variables.tf
+++ aviator/route53/variables.tf
@@ -1,23 +1,21 @@
 variable "zone_name" {
   description = "TLD for Internal Hosted Zone. ( example.com )"
-  type = "string"
+  type        = "string"
 }
 
 variable "zone_environment" {
   description = "Application environment for which this network is being created. one of: ('Development', 'Integration', 'PreProduction', 'Production', 'QA', 'Staging', 'Test')"
-  type = "string"
-  default = "Development"
+  type        = "string"
+  default     = "Development"
 }
 
 variable "env_list" {
   description = "Allowed values in Zone Environment."
-  type = "list"
-  default = ["Development", "Integration", "PreProduction", "Production", "QA", "Staging", "Test"]
+  type        = "list"
+  default     = ["Development", "Integration", "PreProduction", "Production", "QA", "Staging", "Test"]
 }
 
 variable "target_vpc_id" {
   description = "Select Virtual Private Cloud ID. ( vpc-* )"
-  type = "string"
+  type        = "string"
 }
-
-
