1c911a578788e4fc65644123bf4d14d5279378f9
Nate Bohn <nate.bohn@rackspace.com>
2018-06-07 16:50:05 -0500

    requested changes

--- aviator/route53/main.tf
+++ aviator/route53/main.tf
@@ -1,9 +1,17 @@
 # Internal Hosted Zone
 
+locals {
+  env_list = ["Development", "Integration", "PreProduction", "Production", "QA", "Staging", "Test"]
+}
+
 resource "aws_route53_zone" "internal_zone" {
-  name    = "${var.zone_name}"
-  comment = "Hosted zone for ${contains(var.env_list, var.zone_environment) ? var.zone_environment:"Development"}"
-  vpc_id  = "${replace(var.target_vpc_id, "/^vpc-/", "") != var.target_vpc_id ? var.target_vpc_id:""}"
+  name = "${var.zone_name}"
+
+  # Check given environment is in the accepted list, case-sensitive. True=use env, False=use 'Development'
+  comment = "Hosted zone for ${contains(local.env_list, var.environment) ? var.environment:"Development"}"
+
+  # Check to see if input starts with 'vpc-'. True=use input, False=use empty string
+  vpc_id = "${replace(var.target_vpc_id, "/^vpc-/", "") != var.target_vpc_id ? var.target_vpc_id:""}"
 
   tags {
     ServiceProvider = "Rackspace"
--- aviator/route53/variables.tf
+++ aviator/route53/variables.tf
@@ -3,16 +3,10 @@ variable "zone_name" {
   type        = "string"
 }
 
-variable "zone_environment" {
+variable "environment" {
   description = "Application environment for which this network is being created. one of: ('Development', 'Integration', 'PreProduction', 'Production', 'QA', 'Staging', 'Test')"
   type        = "string"
   default     = "Development"
-}
-
-variable "env_list" {
-  description = "Allowed values in Zone Environment."
-  type        = "list"
-  default     = ["Development", "Integration", "PreProduction", "Production", "QA", "Staging", "Test"]
 }
 
 variable "target_vpc_id" {
