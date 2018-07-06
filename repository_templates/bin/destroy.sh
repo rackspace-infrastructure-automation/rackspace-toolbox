c31ed93855d341ef915d8513c0010a5ca9152f96
Martin Smith <martin@mbs3.org>
2018-07-06 08:34:36 -0400

    Remove the statefile from the command to destroy

--- repository_templates/bin/destroy.sh
+++ repository_templates/bin/destroy.sh
@@ -7,9 +7,9 @@ WORKSPACE_DIR="$WORKING_DIR/workspace"
 LAYERS_DIR="$WORKING_DIR/layers"
 
 if [ -f "$WORKSPACE_DIR/changed_layers" ]; then
-  LAYERS=$(cat "$WORKSPACE_DIR/changed_layers" | sort -n)
+  LAYERS=$(cat "$WORKSPACE_DIR/changed_layers" | sort -nr)
 else
-  LAYERS=$(find "$LAYERS_DIR"/* -type d -maxdepth 0 -exec basename '{}' \; | sort -n)
+  LAYERS=$(find "$LAYERS_DIR"/* -type d -maxdepth 0 -exec basename '{}' \; | sort -nr)
 fi
 
 for LAYER in $LAYERS; do
@@ -20,5 +20,5 @@ for LAYER in $LAYERS; do
   (cd "$LAYERS_DIR/$LAYER" && tar xzf "$WORKSPACE_DIR/.terraform.$LAYER.tar.gz" || echo "Did not find a cached .terraform directory")
 
   echo "terraform destroy $LAYER"
-  (cd "$LAYERS_DIR/$LAYER" && terraform destroy -refresh=false -auto-approve "$WORKSPACE_DIR/terraform.$LAYER.plan")
+  (cd "$LAYERS_DIR/$LAYER" && terraform destroy -refresh=false -auto-approve)
 done
