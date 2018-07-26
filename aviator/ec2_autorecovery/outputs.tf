06959c1291ee3ace700251c81c17148285cf2a67
Jon Madison <jonathan.madison@rackspace.com>
2018-07-25 21:35:16 -0500

    move next batch of modules

--- aviator/ec2_autorecovery/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ar_instance_id_list" {
-  description = "List of resulting Instance IDs"
-  value       = ["${split(",",var.secondary_ebs_volume_size != "" ? join(",",coalescelist(aws_instance.mod_ec2_instance_with_secondary_ebs.*.id, list("novalue"))) : join(",",coalescelist(aws_instance.mod_ec2_instance_no_secondary_ebs.*.id, list("novalue"))))}"]
-}
