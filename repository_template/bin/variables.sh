edafdc827921f3c79c730032d0a4540b8a53fa40
Martin Smith <martin@mbs3.org>
2018-08-15 16:36:40 -0400

    More improvements, fixes
    - Circle always wants one of the artifacts to be present, so have plan.sh write to the plan log every time
    - Move the `git fetch` that pulls the current master out to a separate script, run it _before_ adding other SSH keys that could break it

--- repository_template/bin/variables.sh
+++ repository_template/bin/variables.sh
@@ -14,8 +14,10 @@ git fetch origin
 MASTER_REF=$(git rev-parse remotes/origin/master)
 
 # in the last hundred commits, is one of the parents in the current master?
+set +e
 git log --pretty=format:'%H' -n 100 | grep -q "$MASTER_REF"
 UPTODATE=$?
+set -e
 
 if [ $UPTODATE -ne 0 ]
 then
