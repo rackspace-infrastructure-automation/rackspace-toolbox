340efc1d3f6e043af66ba79a8e5c9ad0fac6d025
JP Bochi <jpbochi@protonmail.ch>
2018-09-25 09:12:33 -0600

    un-deprecate check_old.sh

--- repository_template/bin/check_old.sh
+++ repository_template/bin/check_old.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-echo >&2 'check_old.sh is DEPRECATED. Please, remove it from your CI script.'
+# be sure branch is up to date
+git fetch origin
+
+# in the last hundred commits, is one of the parents in the current master?
+if ! (git log --pretty=format:'%H' -n 100 | grep -q "$(git rev-parse remotes/origin/master)"); then
+  echo >&2 'Your branch is not up to date. Exiting.'
+  exit 1
+fi
--- repository_template/bin/lint.sh
+++ repository_template/bin/lint.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-# be sure branch is up to date
-git fetch origin
-MASTER_REF=$(git rev-parse remotes/origin/master)
-
-# in the last hundred commits, is one of the parents in the current master?
-if ! (git log --pretty=format:'%H' -n 100 | grep -q "$MASTER_REF"); then
-  echo >&2 'Your branch is not up to date. Exiting.'
-  exit 1
-fi
-
 . $(dirname $(realpath $0))/variables.sh
 
 terraform fmt -check -diff
