6f06b3c110b7d59867ef02f9aec027b2b449ea2a
JP Bochi <jpbochi@protonmail.ch>
2018-09-24 18:17:00 -0600

    only git fetch on lint.sh

--- repository_template/bin/variables.sh
+++ repository_template/bin/variables.sh
@@ -2,17 +2,8 @@
 
 set -e
 
-# be sure branch is up to date
-git fetch origin
-MASTER_REF=$(git rev-parse remotes/origin/master)
-
-# in the last hundred commits, is one of the parents in the current master?
-if ! (git log --pretty=format:'%H' -n 100 | grep -q "$MASTER_REF"); then
-  echo >&2 'Your branch is not up to date. Exiting.'
-  exit 1
-fi
-
 # standard paths
+MASTER_REF=$(git rev-parse remotes/origin/master)
 WORKING_DIR=$(pwd)
 
 # ensure workspace dir is always present
