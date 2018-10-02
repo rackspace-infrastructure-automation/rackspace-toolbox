6638a93e67d621bc7475794d519cc95524c227ed
JP Bochi <jpbochi@protonmail.ch>
2018-10-02 11:56:21 -0400

    fetch from github.com forcing ~/.ssh/id_rsa

--- repository_template/bin/check_old.sh
+++ repository_template/bin/check_old.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# avoid overridden ssh config for github.com
+if ! (ssh -G rackspace.github.com | grep -q '^hostname github.com$'); then
+  printf 'Host rackspace.github.com\n HostName github.com\n IdentityFile ~/.ssh/id_rsa' >> ~/.ssh/config
+fi
+
 # be sure branch is up to date
-git fetch origin
+git fetch --depth 100 $(git config --get remote.origin.url | sed 's/git@github.com/git@rackspace.github.com/')
 
 # in the last hundred commits, is one of the parents in the current master?
 if ! (git log --pretty=format:'%H' -n 100 | grep -q "$(git rev-parse remotes/origin/master)"); then
