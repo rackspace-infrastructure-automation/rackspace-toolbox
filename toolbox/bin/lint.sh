d2e8d7c786b1fd682dd973b109f8aeab03cfceb4
Martin Smith <martin@mbs3.org>
2018-11-08 16:43:18 +0000

    Disable tuvok for the time being
    Temporarily disable tuvok, while we work through two issues:
    - it's slow on large directory trees
    - we should consider linting only changed files

--- toolbox/bin/lint.sh
+++ toolbox/bin/lint.sh
@@ -4,4 +4,6 @@ set -eu
 . $(dirname $(realpath $0))/variables.sh
 
 terraform fmt -check -diff
-tuvok . || exit 0 # don't fail builds yet
+
+# temporarily disabled
+# tuvok . || exit 0 # don't fail builds yet
