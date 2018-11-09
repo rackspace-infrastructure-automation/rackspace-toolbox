a182b3a0766a7f99917f352c0d6573d917fa6a80
JP Bochi <jpbochi@protonmail.ch>
2018-11-09 13:48:26 +0100

    adding some self documentation

--- toolbox/bin/pull-aws-creds.sh
+++ toolbox/bin/pull-aws-creds.sh
@@ -4,15 +4,31 @@ set -eu
 API_BASE="${API_BASE:-https://github.api.manage.rackspace.com}"
 
 KEY_FILE=${KEY_FILE:-"$HOME/.ssh/id_rsa"}
-FINGERPRINT=$(ssh-keygen -E md5 -lf "$KEY_FILE" | cut -f2 -d' ')
-echo >&2 '>>> Request to be signed with: '"$KEY_FILE"' '"$FINGERPRINT"
 
+fail() {
+  echo >&2 "This tool needs an SSH key to sign a request to $API_BASE."
+  echo >&2 "The key must be configured as a deploy key in your GitHub repository linked to an AWS account."
+  echo >&2 'The default path for the key is "$HOME/.ssh/id_rsa". You can override that by providing a KEY_FILE environment variable.'
+  if [ "${CIRCLECI:-}" = "true" ]; then
+    echo >&2 'It looks like this failure is happening inside CircleCI.'
+    echo >&2 'CircleCI projects are expected to have a deploy key configured in its "Checkout SSH keys" section.'
+  fi
+  exit 1
+}
+
+if [ ! -r "$KEY_FILE" ]; then
+  echo >&2 '> Key file not found: '"$KEY_FILE"
+  fail
+fi
+
+set -o pipefail
+FINGERPRINT=$(ssh-keygen -E md5 -lf "$KEY_FILE" | cut -f2 -d' ')
 REPO_NAME=${REPO_NAME:-$(git config --get remote.origin.url | sed -e 's/^git@github[.]com://' -e 's/^[^\/]*\///' -e 's/[.]git$//')}
 TIME=$(date +%s)
 MESSAGE='{"awsAccountNumber":"'"$TF_VAR_aws_account_id"'","timestamp":"'"$TIME"'","repoName":"'"$REPO_NAME"'"}'
-echo '>>> Requesting credentials: '"$MESSAGE"
 
-set -o pipefail
+echo >&2 "> Requesting credentials for $TF_VAR_aws_account_id. Signing request with $KEY_FILE ($FINGERPRINT)."
+
 SIGNATURE=$(printf $MESSAGE | openssl dgst -sha256 -sign $KEY_FILE | base64 | tr -d '\n')
 
 TEMP_OUTPUT=$(mktemp)
@@ -23,13 +39,13 @@ RESP_CODE=$(curl -sS -XPOST -d "$MESSAGE" \
   "${API_BASE}/v0/aws/credentials")
 
 if [ "$RESP_CODE" != '200' ]; then
-  echo >&2 '>>> Request returned error: '"$RESP_CODE"
+  echo >&2 '> Request returned error: '"$RESP_CODE"
   cat >&2 $TEMP_OUTPUT
   echo >&2
-  exit 1
+  fail
 fi
 
 OUTPUT=${BASH_ENV:-/dev/stdout}
-echo >&2 '>>> Writing response to: '"$OUTPUT"
+echo >&2 '> Writing response to: '"$OUTPUT"
 cat "$TEMP_OUTPUT" >> "$OUTPUT"
 source "$TEMP_OUTPUT"
--- toolbox/bin/variables.sh
+++ toolbox/bin/variables.sh
@@ -14,7 +14,7 @@ check_old() {
 
   # in the last hundred commits, is one of the parents in the current master?
   if ! (git log --pretty=format:'%H' -n 100 | grep -q "$(git rev-parse remotes/origin/master)"); then
-    echo >&2 'Your branch is not up to date. Exiting...'
+    echo >&2 'Your branch is not up to date with remotes/origin/master. Exiting...'
     exit 1
   else
     echo 'Your branch is up to date. Proceeding...'
