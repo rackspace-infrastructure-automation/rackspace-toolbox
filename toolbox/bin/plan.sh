97b0af0f610c40436b8302af8011e0bf492d7612
JP Bochi <jpbochi@protonmail.ch>
2018-12-05 14:40:53 +0100

    saves one log of all plans

--- toolbox/bin/plan.sh
+++ toolbox/bin/plan.sh
@@ -16,27 +16,23 @@ if [ -z "$CHANGED_LAYERS" ]; then
 fi
 
 for LAYER in $CHANGED_LAYERS; do
-  echo "> Planning layer: $LAYER"
+  PLANS_LOG="$WORKSPACE_DIR/terraform.plans.log"
+  echo "> Planning layer: $LAYER" | tee -a "$PLANS_LOG"
 
   # ensure even deleted layers are plannable
   if [ ! -d "$LAYERS_DIR/$LAYER" ]; then
-    echo "> Layer directory $LAYERS_DIR/$LAYER was not found, creating an empty version."
+    echo "> Layer directory $LAYERS_DIR/$LAYER was not found, creating an empty version." | tee -a "$PLANS_LOG"
     mkdir -p "$LAYERS_DIR/$LAYER/.terraform"
     touch "$LAYERS_DIR/$LAYER/deleted.tf"
   fi
 
-  pushd "$LAYERS_DIR/$LAYER"
-  set -x
-  terraform init -no-color -input=false -backend=true -backend-config="bucket=$TF_STATE_BUCKET" -backend-config="region=$TF_STATE_REGION" -backend-config="encrypt=true"
+  cd "$LAYERS_DIR/$LAYER"
+  (set -x && terraform init -no-color -input=false -backend=true -backend-config="bucket=$TF_STATE_BUCKET" -backend-config="region=$TF_STATE_REGION" -backend-config="encrypt=true")
 
   # cache .terraform during the plan
   tar -czf "$WORKSPACE_DIR/.terraform.$LAYER.tar.gz" .terraform
 
-  terraform plan -no-color -input=false -out="$WORKSPACE_DIR/terraform.$LAYER.plan" | tee /dev/stdout | sed -n '/---/,/---/p' > "$WORKSPACE_DIR/$LAYER.plan.txt"
-  set +x
-  popd
-
-  # for debugging, show these files exist
-  ls -la "$WORKSPACE_DIR/.terraform.$LAYER.tar.gz"
-  ls -la "$WORKSPACE_DIR/terraform.$LAYER.plan"
+  (set -x && terraform plan -no-color -input=false -out="$WORKSPACE_DIR/terraform.$LAYER.plan") | tee /dev/stdout | sed -n '/---/,/---/p' >> "$PLANS_LOG"
 done
+
+ls -lh "$WORKSPACE_DIR/"
